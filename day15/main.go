04995c511e7cae39a822860e92c2571e6af77886
jimmykodes <jkeith@kochava.com>
2022-02-15 11:28:29 -0800

    day15 part 1

--- day15/main.go
+++ day15/main.go
@@ -4,13 +4,96 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
+type Stack []*Node
+
+func (s Stack) Len() int {
+	return len(s)
+}
+
+func (s Stack) Less(i, j int) bool {
+	return s[i].TotalRisk > s[j].TotalRisk
+}
+
+func (s Stack) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s *Stack) Push(x *Node) {
+	*s = append(*s, x)
+	sort.Sort(*s)
+}
+
+func (s *Stack) Pop() *Node {
+	o := *s
+	n := len(o) - 1
+	node := o[n]
+	o[n] = nil
+	*s = o[0:n]
+	return node
+}
+
+func (s *Stack) Peek() *Node {
+	o := *s
+	return o[len(*s)-1]
+}
+
 func main() {
-	board := loadBoard("test.txt")
-	fmt.Println(board)
+	board := loadBoard("input.txt")
+	stack := Stack([]*Node{board[0][0]})
+	step := 0
+	current := stack.Pop()
+	current.Visited = true
+	for !current.IsEnd {
+		step++
+		via := current.Via
+		previousRisk := 0
+		if via != nil {
+			previousRisk = via.TotalRisk
+		}
+		for _, n := range []*Node{current.Left, current.Right, current.Top, current.Bottom} {
+			if n == nil || n == via {
+				continue
+			}
+			totalRisk := previousRisk + current.Risk + n.Risk
+			if n.Visited {
+				if totalRisk < n.TotalRisk {
+					// less risky through this path, update things
+					n.Via = current
+					n.TotalRisk = totalRisk
+					sort.Sort(stack)
+				}
+				continue
+			}
+			n.Via = current
+			n.TotalRisk = totalRisk
+			n.Visited = true
+			stack.Push(n)
+		}
+		current = stack.Pop()
+	}
+	fmt.Println("completed in steps:", step)
+	r := 0
+	for !current.IsStart {
+		current.Path = true
+		r += current.Risk
+		current = current.Via
+	}
+	fmt.Println("total risk:", r)
+	for _, nodes := range board {
+		for _, node := range nodes {
+			char := "-"
+			if node.Path {
+				char = strconv.Itoa(node.Risk)
+			}
+			fmt.Print(char)
+		}
+		fmt.Println()
+	}
 }
 
 func loadBoard(filename string) [][]*Node {
@@ -37,6 +120,12 @@ func loadBoard(filename string) [][]*Node {
 				Risk:      r,
 				SquareMag: math.Pow(float64(finalX-x), 2) + math.Pow(float64(finalY-y), 2),
 			}
+			if y == 0 && x == 0 {
+				n.IsStart = true
+			}
+			if y == finalY && x == finalX {
+				n.IsEnd = true
+			}
 			b[y][x] = n
 			if x > 0 {
 				// connect left
@@ -57,11 +146,22 @@ func loadBoard(filename string) [][]*Node {
 type Node struct {
 	X         int
 	Y         int
+	TotalRisk int
 	Risk      int
 	SquareMag float64
-	Left      *Node
-	Right     *Node
-	Top       *Node
-	Bottom    *Node
-	Via       *Node
+
+	Left   *Node
+	Right  *Node
+	Top    *Node
+	Bottom *Node
+	Via    *Node
+
+	Visited bool
+	IsStart bool
+	IsEnd   bool
+	Path    bool
+}
+
+func (n Node) String() string {
+	return fmt.Sprintf("(%d, %d) %d %d", n.X, n.Y, n.Risk, n.TotalRisk)
 }
